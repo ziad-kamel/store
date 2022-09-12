2c78807cbe904bae57e64dc8344809abf20a1468
zika184 <zika184@github.com>
2022-09-12 15:53:17 +0200

    add prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node":true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        'prettier'
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        'prettier',
+    ],
+    "rules": {
+        'prettier/prettier':2,
+        // quotes:['error','never'],
+        'no-console': 1,
+        'no-var':'error',
+        'prefer-const':'error',
+    },
+};
